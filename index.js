9888e75f11fb5fac1faa029c2fa07d954943dc1f
kp423 <1parkinsokei@hdsb.ca>
2024-09-12 12:39:12 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log ("Welcome to Keith's Calculator. Here you are able to use this calculator to add (+), subtract (-), multply(*) and divide (/)")
+
+x = prompt("First number?")
+operator = prompt("Which operator")
+y = prompt("Second number?")
+
+if (operator = +) {
+    function add(x, y)
+    return x + y;
+}
+
+function subtract(x, y) { 
+    return x - y;
+}
+
+function multiply(x, y) { 
+    return x * y;
+}
+
+function divide(x, y) { 
+    return x / y;
+}
