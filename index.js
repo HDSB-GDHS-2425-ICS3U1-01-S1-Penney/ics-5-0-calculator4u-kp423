04a215afd0c25af50d49beee3f9bf12e01414793
kp423 <1parkinsokei@hdsb.ca>
2024-09-13 12:33:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 console.log ("Welcome to Keith's Calculator. Here you are able to use this calculator to add (+), subtract (-), multply(*) and divide (/)")
 
-x = prompt("First number?")
+x = Number(prompt("First number?"));
 operator = prompt("Which operator")
-y = prompt("Second number?")
+y = Number(prompt("Second number?"));
 
-if (operator = +) {
-    function add(x, y)
-    return x + y;
+if (operator=="+") {
+    result = add(); 
+} else if (operator=="-") {
+    result = subtract(); 
+} else if (operator=="*") {
+    result = multiply();
+} else if (operator=="/") {
+    result = divide();
+}
+console.log(result)
+ 
+function add() {
+return x+y;
+ }
+
+
+function subtract() { 
+    return x-y;
 }
 
-function subtract(x, y) { 
-    return x - y;
+function multiply() { 
+    return x*y;
 }
 
-function multiply(x, y) { 
-    return x * y;
+function divide() { 
+    return x/y;
 }
 
-function divide(x, y) { 
-    return x / y;
+const operator = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] {
+    result = console.log("Error")
 }
